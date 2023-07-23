5a31135a2cb616b90f3d10b189c8a2ff45effd26
SheshanRavihansa <ravihansasheshan@gmail.com>
2023-07-23 12:08:21 +0530

    Route Home Link

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <a href="{{route('web')}}" class="">
+                            <a href="{{route('home')}}" class="">
                                 <h3 class="text-primary">FibroTech</h3>
                             </a>
                             <h3>Sign Up</h3>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -17,10 +17,10 @@
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <a href="{{ route('web') }}" class="">
+                            <a href="{{ route('home') }}" class="">
                                 <h3 class="text-primary">FibroTech</h3>
                             </a>
-                            <h3>Forgot Password</h3>
+                            <h3>Reset Password</h3>
                         </div>
                         <!-- Alerts -->
                         <div>
