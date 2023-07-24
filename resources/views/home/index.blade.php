c5c4b9092eebbe0e805206733690b3c7d10b54b9
SheshanRavihansa <ravihansasheshan@gmail.com>
2023-07-25 00:22:22 +0530

    spatie permission install

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.web')
+@section('main-content')
+    @auth
+        <h1>You are logged in !</h1>
+        <h1><a href="{{ route('logout') }}">Logout</a></h1>
+    @else
+        <h1>Please <a href="{{ route('login') }}">LogIn</a> !</h1>
+    @endauth
+@endsection
