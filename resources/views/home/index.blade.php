17dc3f51a1426c8a18cbd2ff77f364fe8238b576
SheshanRavihansa <ravihansasheshan@gmail.com>
2023-07-25 14:02:19 +0530

    admin dashboard

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,13 @@
 @section('main-content')
     @auth
         <h1>You are logged in !</h1>
+        @role('admin')
+            <h1>I am an admin!</h1>
+            <h1><a href="{{ route('admin.dashboard') }}">dashboard</a></h1>
+        @else
+            <h1>I am an user...</h1>
+        @endrole
+
         <h1><a href="{{ route('logout') }}">Logout</a></h1>
     @else
         <h1>Please <a href="{{ route('login') }}">LogIn</a> !</h1>
