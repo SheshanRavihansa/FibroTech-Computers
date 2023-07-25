ed29a7d2e7a4878d0cb77f87cc8e2a9273f56965
SheshanRavihansa <ravihansasheshan@gmail.com>
2023-07-25 12:18:05 +0530

    permissions seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,6 +19,14 @@ class AdminSeeder extends Seeder
             'email' => 'admin@email.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
+        ])->assignRole('admin');
+
+        User::create([
+            'firstName' => 'User',
+            'lastName' => 'User',
+            'email' => 'user@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('user');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            PermissionSeeder::class,
             AdminSeeder::class,
         ]);
     }
