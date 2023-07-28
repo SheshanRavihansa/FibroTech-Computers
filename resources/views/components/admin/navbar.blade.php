af2b0b53e4731ecb3b99458eedf2b9adf4503b84
SheshanRavihansa <ravihansasheshan@gmail.com>
2023-07-28 21:41:50 +0530

    navbar links

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
-    <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
+    <a href="{{route('home')}}" class="navbar-brand d-flex d-lg-none me-4">
         <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
     </a>
     <a href="#" class="sidebar-toggler flex-shrink-0">
@@ -7,7 +7,7 @@
     </a>
     <div class="navbar-nav align-items-center ms-auto">
         <div class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+            <a href="{{route('home')}}" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 <img class="rounded-circle me-lg-2" src="/storage/user.png" alt=""
                     style="width: 40px; height: 40px;">
                 <span class="d-none d-lg-inline-flex">{{ auth()->user()->firstName . ' ' . auth()->user()->lastName }}
